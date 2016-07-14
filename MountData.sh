145c2300017569398a61234c5b8c4225a0e7ef97
egamcr15 <emmanuel.guzman15@gmail.com>
2016-07-14 13:43:58 -0500

    Update MountData.sh

--- MountData.sh
+++ MountData.sh
@@ -5,11 +5,10 @@ Primary="p"
 Numero=1
 Write=w
 
-sudo grep SCSI /var/log/syslog
 sudo fdisk /dev/sdc
-echo $NewPartition | -ne '\n'
-echo $Primary | -ne '\n'
-echo $Numero |-ne '\n' | -ne '\n' | -ne '\n'
+echo $NewPartition |'\n'
+echo $Primary |'\n'
+echo $Numero |'\n' |'\n' |'\n'
 echo $Write 
 sudo mkfs -t ext4 /dev/sdc1
 sudo mount /dev/sdc1 /datadrive
