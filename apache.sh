41444d711492b10122c8defad15282004954de40
egamcr15 <emmanuel.guzman15@gmail.com>
2016-07-14 15:50:32 -0500

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 apt-get -y update
-
 # install Apache2
-apt-get -y install apache2 
-
+apt-get -y install apache2
 # write some HTML
-echo \<center\>\<h1\>My Demo App\</h1\>\<br/\>\</center\> > /var/www/html/demo.html
-
+#echo \<center\>\<h1\>My Demo App\</h1\>\<br/\>\</center\> > /var/www/html/demo.html
 # restart Apache
-apachectl restart
+#apachectl restart
