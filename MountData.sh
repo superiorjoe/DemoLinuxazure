282f406ac1d55bbb28494711571e7ae095e788ec
egamcr15 <emmanuel.guzman15@gmail.com>
2016-07-19 13:42:01 -0500

    Add files via upload

---
+++ MountData.sh
@@ -0,0 +1,12 @@
+
+#!/bin/bash
+NewPartition='n'
+Primary='p'
+Numero=1
+Write='w'
+sudo grep SCSI /var/log/syslog
+sudo fdisk /dev/sdc
+sudo mkfs -t ext4 /dev/sdc1
+sudo mkdir /datadrive
+sudo mount /dev/sdc1 /datadrive 
+ls /datadrive/ 
